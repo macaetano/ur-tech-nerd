7fb3325f061e1c88ddaaa677a936e373dc2f65ce
Matheus De Assis Caetano <matheusdeassiscaetano@gmail.com>
2021-02-08 08:23:46 -0300

    updated nav ( added current page marker )

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 const StyledNav = styled.div`
   display: flex;
@@ -11,28 +12,40 @@ const NavItem = styled.h3`
   padding: 1rem;
   a {
     text-decoration: none;
+    text-transform: uppercase;
     color: var(--black);
+    border-bottom-width: 3px;
+    border-bottom-style: solid;
+    border-bottom-color: ${(props) =>
+      props.isActive ? "var(--red)" : "transparent"};
   }
   a:hover {
-    border-bottom: 3px solid var(--black);
+    border-bottom-color: var(--red);
   }
 `;
 
-const Nav = () => (
-  <StyledNav>
-    <NavItem>
-      <Link href="/Sell">Sell</Link>
-    </NavItem>
-    <NavItem>
-      <Link href="/Orders">Orders</Link>
-    </NavItem>
-    <NavItem>
-      <Link href="/Account">Account</Link>
-    </NavItem>
-    <NavItem>
-      <Link href="/Cart">Cart</Link>
-    </NavItem>
-  </StyledNav>
-);
+const Nav = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <StyledNav>
+      <NavItem isActive={pathname === "/Buy"}>
+        <Link href="/Buy">Buy</Link>
+      </NavItem>
+      <NavItem isActive={pathname === "/Sell"}>
+        <Link href="/Sell">Sell</Link>
+      </NavItem>
+      <NavItem isActive={pathname === "/Orders"}>
+        <Link href="/Orders">Orders</Link>
+      </NavItem>
+      <NavItem isActive={pathname === "/Account"}>
+        <Link href="/Account">Account</Link>
+      </NavItem>
+      <NavItem isActive={pathname === "/Cart"}>
+        <Link href="/Cart">Cart</Link>
+      </NavItem>
+    </StyledNav>
+  );
+};
 
 export default Nav;
